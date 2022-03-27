6f0633f6f36ea43c6dd8f3459e53460c0cb9f983
Brenden Moore <bmoore@accusoft.com>
2022-03-27 10:20:23 -0400

    details styling and back arrow

--- src/components/TodoDetails.jsx
+++ src/components/TodoDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { getTodoById, getUserById } from '../todoService'
 
 function TodoDetails() {
@@ -17,13 +17,15 @@ function TodoDetails() {
 		fetchData()
 	}, [])
 
-	return (
-		<div>TodoDetails
-			<p>{id}</p>
-			<p>{todo ? todo.title : "Loading"}</p>
-			<p>{user ? user.name : "Loading User"}</p>
+	return user ? (
+		<div className="">
+			<Link to="/" className="text-4xl">←</Link>
+			<h1 className="text-2xl mb-5 font-bold">{todo.title}</h1>
+			<p><b>Id:</b> {id}</p>
+			<p><b>Completed:</b> {todo.completed.toString()}</p>
+			<p><b>Created By:</b> {user.name}</p>
 		</div>
-	)
+	) : <p>Loading</p>
 }
 
 export default TodoDetails
